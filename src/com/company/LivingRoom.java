0befe46521319d0ff71a744422ee353e37cb5d65
sanjanenic <sanjanenicbl@gmail.com>
2022-03-02 10:55:30 +0100

    erste projekt

---
+++ src/com/company/LivingRoom.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class LivingRoom {
+
+    public static void main(String[] args) {
+	// write your code here
+        double squareMeters = 45.4;
+        boolean hasTv = true;
+        boolean hasCouch = true;
+        int windows = 3;
+        System.out.println (squareMeters);
+        System.out.println (hasTv);
+        System.out.println (hasCouch);
+        System.out.println (windows);
+
+    }
+}
